c073a5f6f72b3b22b69540cec76b565ae348c9e0
Aleksandr Staetsky <myexploretime@gmail.com>
2019-05-21 11:58:32 +0300

    logo

--- js/script.js
+++ js/script.js
@@ -11,6 +11,32 @@ $(document).ready(function () {
   });
 
 
+//   button to top
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() != 0) {
+
+            $('#toTop').fadeIn();
+
+        } else {
+
+            $('#toTop').fadeOut();
+
+        }
+
+    });
+
+    $('#toTop').click(function () {
+
+        $('body,html').animate({ scrollTop: 0 }, 800);
+
+    });
+
+});
+
+
 // carousel setup 
 
 $(document).ready(function(){
@@ -67,6 +93,7 @@ $(document).ready(function(){
 		$("#"+tab_id).addClass('current');
 	})
 })
+
 
 
 
